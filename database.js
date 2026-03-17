a66b1001b6af8e8fb1967cb07dfd0f8b5f2d7341
protower321 <ruanvan651@gmail.com>
2026-03-17 00:07:40 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,13 @@
-
+let db = {
+  users: [
+    {
+      username: "admin",
+      password: "admin123",
+      rank: "General",
+      status: "Active"
+    }
+  ],
+  applications: [],
+  logs: [],
+  discipline: []
+};
